c561288f45032ab0bca793850b3228bbf4983b51
Nadin Tamer <nadin.tamer@gmail.com>
2021-07-07 15:36:43 -0700

    Removed labels from tab bar navigation; added number of likes & comments list to Parse model

--- app/src/main/java/com/example/instagram/models/Post.java
+++ app/src/main/java/com/example/instagram/models/Post.java
@@ -6,12 +6,16 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import java.util.List;
+
 @ParseClassName("Post")
 public class Post extends ParseObject {
 
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_COMMENTS = "comments";
+    private static final String KEY_LIKES = "numLikes";
 
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
@@ -35,5 +39,37 @@ public class Post extends ParseObject {
 
     public void setUser(ParseUser user) {
         put(KEY_USER, user);
+    }
+
+    public int getNumLikes() {
+        return getInt(KEY_LIKES);
+    }
+
+    public void setNumLikes(int likes) {
+        put(KEY_LIKES, likes);
+    }
+
+    public void addLike() {
+        int numLikes = getNumLikes();
+        setNumLikes(numLikes + 1);
+    }
+
+    public void removeLike() {
+        int numLikes = getNumLikes();
+        setNumLikes(numLikes - 1);
+    }
+
+    public List<String> getComments() {
+        return getList(KEY_COMMENTS);
+    }
+
+    public void setComments(List<String> comments) {
+        put(KEY_COMMENTS, comments);
+    }
+
+    public void addComment(String comment) {
+        List<String> comments = getComments();
+        comments.add(comment);
+        setComments(comments);
     }
 }
