bb7f5a1fd1b11b829d3ccc2c83999c79ea380fdd
Nadin Tamer <nadin.tamer@gmail.com>
2021-07-09 10:24:31 -0700

    User can upload profile photo from gallery

--- app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
@@ -1,5 +1,10 @@
 package com.example.instagram.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,10 +13,12 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -24,15 +31,19 @@ import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import org.parceler.Parcels;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
     private static final String TAG = "FeedFragment";
+    private static final int PICK_PHOTO_CODE = 42;
     private FragmentProfileBinding binding;
     private List<Post> posts;
     private PostsAdapter adapter;
@@ -75,20 +86,14 @@ public class ProfileFragment extends Fragment {
         binding.rvPosts.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         binding.tvUsername.setText(user.getUsername());
-
-        ParseFile imageFile = user.getParseFile("profilePhoto");
-        if (imageFile != null) {
-            Glide.with(getActivity())
-                    .load(imageFile.getUrl())
-                    .circleCrop()
-                    .into(binding.ivProfilePhoto);
-        }
+        fetchProfilePhoto();
 
         if (user == ParseUser.getCurrentUser()) {
             binding.ivProfilePhoto.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Toast.makeText(getActivity(), "Editing profile photo!", Toast.LENGTH_SHORT).show();
+                    onPickPhoto();
                 }
             });
         }
@@ -141,5 +146,83 @@ public class ProfileFragment extends Fragment {
                 binding.swipeContainer.setRefreshing(false);
             }
         });
+    }
+
+    // Trigger gallery selection for a photo
+    public void onPickPhoto() {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            startActivityForResult(intent, PICK_PHOTO_CODE);
+        }
+    }
+
+    public Bitmap loadFromUri(Uri photoUri) {
+        Bitmap image = null;
+        try {
+            // check version of Android on device
+            if (Build.VERSION.SDK_INT > 27){
+                // on newer versions of Android, use the new decodeBitmap method
+                ImageDecoder.Source source = ImageDecoder.createSource(getActivity().getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            } else {
+                // support older versions of Android by using getBitmap
+                image = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), photoUri);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ((data != null) && requestCode == PICK_PHOTO_CODE) {
+            Uri photoUri = data.getData();
+
+            // Load the image located at photoUri into selectedImage
+            Bitmap selectedImage = loadFromUri(photoUri);
+            saveProfilePhoto(selectedImage);
+        }
+    }
+
+    // https://stackoverflow.com/questions/27146665/putting-image-from-gallery-in-parsefile-in-android
+    public void saveProfilePhoto(Bitmap map) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        // Compress image to lower quality scale 1 - 100
+        map.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] image = stream.toByteArray();
+
+        // Create the ParseFile
+        ParseFile file  = new ParseFile("profilePhoto.jpeg", image);
+        // Upload the image into Parse Cloud
+        ParseUser user = ParseUser.getCurrentUser();
+        user.put("profilePhoto", file);
+        user.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Toast.makeText(getActivity(), "Error uploading profile photo!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                fetchProfilePhoto();
+            }
+        });
+    }
+
+    private void fetchProfilePhoto() {
+        ParseFile imageFile = user.getParseFile("profilePhoto");
+        if (imageFile != null) {
+            Glide.with(getActivity())
+                    .load(imageFile.getUrl())
+                    .circleCrop()
+                    .into(binding.ivProfilePhoto);
+        }
     }
 }
