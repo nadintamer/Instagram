6909725ca885135fa0acfbf9c2a794b0c4eabfaa
Nadin Tamer <nadin.tamer@gmail.com>
2021-07-09 11:27:51 -0700

    Endless scrolling works

--- app/src/main/java/com/example/instagram/fragments/FeedFragment.java
+++ app/src/main/java/com/example/instagram/fragments/FeedFragment.java
@@ -8,6 +8,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.util.Log;
@@ -18,6 +19,7 @@ import android.view.ViewGroup;
 import com.example.instagram.adapters.PostsAdapter;
 import com.example.instagram.databinding.FragmentFeedBinding;
 import com.example.instagram.models.Post;
+import com.example.instagram.utilities.EndlessRecyclerViewScrollListener;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
@@ -28,9 +30,11 @@ import java.util.List;
 public class FeedFragment extends Fragment {
 
     private static final String TAG = "FeedFragment";
+    private static final int NUM_POSTS_TO_LOAD = 20;
     private FragmentFeedBinding binding;
     private List<Post> posts;
     private PostsAdapter adapter;
+    private EndlessRecyclerViewScrollListener scrollListener;
 
     public FeedFragment() {
         // Required empty public constructor
@@ -51,14 +55,27 @@ public class FeedFragment extends Fragment {
         posts = new ArrayList<>();
         adapter = new PostsAdapter(this, posts, true);
 
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         binding.rvPosts.setAdapter(adapter);
-        binding.rvPosts.setLayoutManager(new LinearLayoutManager(getActivity()));
+        binding.rvPosts.setLayoutManager(linearLayoutManager);
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                queryPosts(totalItemsCount);
+            }
+        };
+        // Adds the scroll listener to RecyclerView
+        binding.rvPosts.addOnScrollListener(scrollListener);
 
         // Setup refresh listener which triggers new data loading
         binding.swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                queryPosts();
+                adapter.clear();
+                queryPosts(0);
+                binding.swipeContainer.setRefreshing(false);
             }
         });
         // Configure the refreshing colors
@@ -67,16 +84,17 @@ public class FeedFragment extends Fragment {
                 android.R.color.holo_orange_light,
                 android.R.color.holo_red_light);
 
-        queryPosts();
+        queryPosts(0);
     }
 
-    private void queryPosts() {
+    private void queryPosts(int numToSkip) {
         // specify what type of data we want to query - Post.class
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         // include data referred by user key
         query.include(Post.KEY_USER);
         // limit query to latest 20 items
-        query.setLimit(20);
+        query.setLimit(NUM_POSTS_TO_LOAD);
+        query.setSkip(numToSkip);
         // order posts by creation date (newest first)
         query.addDescendingOrder("createdAt");
         // start an asynchronous call for posts
@@ -95,9 +113,7 @@ public class FeedFragment extends Fragment {
                 }
 
                 // save received posts to list and notify adapter of new data
-                adapter.clear();
                 adapter.addAll(posts);
-                binding.swipeContainer.setRefreshing(false);
             }
         });
     }
